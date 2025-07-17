b4b9d0e900e1116a9e4c3ad54164d2f9876838b3
francesco mansi <f.mansi@socialwebsolutions.it>
2025-07-17 12:52:22 +0200

    aggiunta file

--- index.php
+++ index.php
@@ -1,11 +1,73 @@
 <?php
+//Sequenza
+// echo $frase1;
+// echo $frase2;
 
-$stringa = 'Francesco';
-$intero = 90;
+// $frase1 = 'CIao mi chiamo Francesco';
+// $frase2 = 'Piacere';
 
-$a = 90;
-$b = 12;
+//Selezione
+// if (2 > 1) {
+//     echo "Maggiore";
+// }
+
+$age = 18; // intero
 
 
-echo "La somma è " . $a + $b . "\n";
-echo "La somma è " . $a + $b . "\n";
+//controlla casi limite
+//controlla casi limite2
+//controlla casi limite3
+//controlla casi limite4
+
+//controlla casi eccezionali
+
+//esegui cai normali
+if ($age > 120 or $age < 1) {
+    echo "Eta Non corretta";
+} else {
+    if ($age > 18) {
+        echo "SI, ha puiu di 18 anni.";
+    } elseif ($age < 18) {
+        echo "No, ha meno di 18 anni.";
+    } else {
+        echo "Ha esattamente 18 anni.";
+    }
+}
+
+//Switch
+
+
+switch ($variable) {
+    case $variable > 18:
+        echo "SI, ha puiu di 18 anni.";
+        break;
+    case $variable < 18:
+        echo "Hai meno di 18 anni.";
+        break;
+    case $variable == 18:
+        echo "Hai esattamente 18 anni.";
+        break;
+    default:
+        echo "Eta Non corretta";
+        break;
+}
+
+$role = 'admin';
+
+switch ($role) {
+    case 'admin':
+        echo "SI, ha puiu di 18 anni.";
+        break;
+    case 'insalata':
+        echo "SI, ha puiu di 18 anni.";
+        break;
+    case $variable < 18:
+        echo "Hai meno di 18 anni.";
+        break;
+    case $variable == 18:
+        echo "Hai esattamente 18 anni.";
+        break;
+    default:
+        echo "Eta Non corretta";
+        break;
+}
